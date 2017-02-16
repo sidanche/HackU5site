87b1cb882ecd3f42553c231265a1d2afd502d466
Sid Anche <sidanche@gmail.com>
2017-02-16 18:08:39 -0500

    returns rating after every character

--- pages/index.js
+++ pages/index.js
@@ -4,25 +4,42 @@ import Link from 'next/prefetch'
 import 'isomorphic-fetch'
 
 export default class extends React.Component {
-  static async getInitialProps () {
-    // eslint-disable-next-line no-undef
-    const res = await fetch('https://api.github.com/repos/zeit/next.js')
-    const json = await res.json()
-    return { stars: json.stargazers_count }
-  }
+constructor(props) {
+   super(props);
+   this.state = {
+     value: '',
+     response: ''
+   };
 
-submit(e){
+ }
+
+async querySender(value){
+    const myLoad = JSON.stringify({input: {csvInstance: [value]}})
+    const url = 'https://www.googleapis.com/prediction/v1.6/projects/yelppredictor/trainedmodels/prediction-model/predict?'
+    const res = await fetch(url, {method: 'POST', 
+    	headers: {'Authorization': 'Bearer ya29.Glv0A5sZTaVrwgIqNCGkGeNlwr6e09meu-RbJxHiWGCPF8In3vwi5B7xPMbXCQEj3NRZ8bWEyffKnoZCecyWoQXHZjyRSKThpuXo-k6U8T0vP_QsGBR6U-YirbRR',
+    	'Content-Type': 'application/json'} ,
+    	body: myLoad})
+    const json = await res.json()
+    console.log(json.outputLabel, value)
+    this.setState({response: json.outputLabel})
+
+
+}
+submit=(e)=>{
 	e.preventDefault()
-	console.log("submitted")
+	this.setState({value: e.target.value})
+	this.querySender(e.target.value)
+
 }
 
   render () {
     return (
       <div>
-      	<form onSubmit={this.submit}>
+      <p>{this.state.response}</p>
+      	<form>
  		 Review Entry:
- 		<textarea name="Text1" cols="40" rows="5"></textarea>
-  		<input type="text" name="reviewentry" placeholder="Enter Review Here"/>
+ 		<textarea name="Text1" onChange={this.submit} value={this.state.value} cols="40" rows="5" placeholder="Enter Review Here"></textarea>
   		<input type="submit" value="Submit"/>
 		</form>
       </div>
