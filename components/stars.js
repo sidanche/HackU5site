85ad971420c8e233af396be4e35a8e956346b6c9
Sid Anche <sidanche@gmail.com>
2017-02-16 21:58:22 -0500

    Basic Web UI completed

---
+++ components/stars.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+export default class extends React.Component {
+  render() {
+    return (
+      <div>
+        <style>
+        {`
+          .flex {
+            display: -webkit-flex;
+            display: flex;
+            -webkit-flex-direction: row;
+            flex-direction: row;
+            justify-content: center;
+          }
+          .stars {
+              width: 100%;
+              max-width: 400px;
+              margin: 0 auto 1rem;
+          }
+          .star {
+            -webkit-clip-path: polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%);
+            clip-path: polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%);
+            width: 80px;
+            height: 80px;
+            background-color: #ECE5F0;
+            -moz-transition: background-color 400ms cubic-bezier(.4,0,.2,1);
+            -webkit-transition: background-color 400ms cubic-bezier(.4,0,.2,1);
+            -o-transition: background-color 400ms cubic-bezier(.4,0,.2,1);
+            transition: background-color 400ms cubic-bezier(.4,0,.2,1);
+          }
+          .stars.one div:nth-child(1), 
+          .stars.two div:nth-child(-n+2), 
+          .stars.three div:nth-child(-n+3),
+          .stars.four div:nth-child(-n+4),
+          .stars.five div 
+          {
+              background-color: rgb(240, 179, 35);
+              -moz-transition: background-color 400ms cubic-bezier(.4,0,.2,1);
+              -webkit-transition: background-color 400ms cubic-bezier(.4,0,.2,1);
+              -o-transition: background-color 400ms cubic-bezier(.4,0,.2,1);
+              transition: background-color 400ms cubic-bezier(.4,0,.2,1);
+          }    
+        `}
+        </style>
+        <div className={this.props.stars}>
+          <div className="star"/>
+          <div className="star"/>
+          <div className="star"/>
+          <div className="star"/>
+          <div className="star"/>
+        </div>
+        </div>
+      )
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-
+import Head from 'next/head'
 import React from 'react'
 import Link from 'next/prefetch'
+import Stars from '../components/stars'
 import 'isomorphic-fetch'
 
 export default class extends React.Component {
@@ -8,7 +9,7 @@ constructor(props) {
    super(props);
    this.state = {
      value: '',
-     response: '' 
+     response: 'one' 
    };
 
  }
@@ -17,7 +18,7 @@ async querySender(value){
     const myLoad = JSON.stringify({input: {csvInstance: [value]}})
     const url = 'https://www.googleapis.com/prediction/v1.6/projects/yelppredictor/trainedmodels/prediction-model/predict?'
     const res = await fetch(url, {method: 'POST', 
-    	headers: {'Authorization': 'Bearer ya29.Glv0A5sZTaVrwgIqNCGkGeNlwr6e09meu-RbJxHiWGCPF8In3vwi5B7xPMbXCQEj3NRZ8bWEyffKnoZCecyWoQXHZjyRSKThpuXo-k6U8T0vP_QsGBR6U-YirbRR',
+    	headers: {'Authorization': 'Bearer ya29.Glv1A4u-g1yAk6hRLPr5lgw5D4IRM8fsPe3vleMdlcYTYx-FFIUBuDWeovoVqISCrm6vwwxTx801169dNotYsVkE4SP796rWsZqvRFmCv46fJ1Rou2L0G_gERzK9',
     	'Content-Type': 'application/json'} ,
     	body: myLoad})
     const json = await res.json()
@@ -34,14 +35,68 @@ submit=(e)=>{
 }
 
   render () {
+  	const stars=`stars flex ${this.state.response}`
     return (
       <div>
-      <p>{this.state.response}</p>
-      	<form>
- 		 Review Entry:
- 		<textarea name="Text1" onChange={this.submit} value={this.state.value} cols="40" rows="5" placeholder="Enter Review Here"></textarea>
-  		<input type="submit" value="Submit"/>
-		</form>
+      <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+         <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Nunito:200,400" />
+      </Head>
+
+      <style>{`
+    html, body {
+    	background: #F1FFE7;
+    	margin: 0;
+    	padding: 0;
+	}
+	
+	main, header{
+		max-width: 1040px;
+		width: auto;
+		padding: 0 1rem;
+		margin: 0 auto;
+	}
+
+	h1{
+		font-family: Nunito, sans-serif;
+		font-size: 52px;
+		font-weight: 200;
+		text-align: center;
+	}
+    textarea{
+    	display: block;
+      	margin: 0 auto;
+      	padding: 2rem;
+      	font-weight: 400;
+      	font-size: 36px;
+      	border: none;
+    	overflow: auto;
+    	outline: none;
+    	width: 100%;
+    	max-width: 880px;
+    	min-height: 40vh;
+    	resize: vertical;
+    	-webkit-box-shadow: none;
+    	-moz-box-shadow: none;
+    	box-shadow: none;
+    	-webkit-box-sizing: border-box;
+    	-moz-box-sizing: border-box;
+    	box-sizing: border-box;
+    }
+    textarea:focus{
+    	border: none;
+    	outline: none;
+    }
+
+
+    `}</style>
+    	<header>
+    	<h1>Yelp Review Project</h1>
+    	</header>
+      <main>
+      <Stars stars={stars} />
+ 		<textarea name="Text1" onChange={this.submit} value={this.state.value} placeholder="Enter Review Here"></textarea>
+      </main>
       </div>
     )
   }
